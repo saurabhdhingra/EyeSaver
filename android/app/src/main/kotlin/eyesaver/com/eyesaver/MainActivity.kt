b6c996451b7fe4a6514d48aeffe7977a6cf2fab4
saurabhdhingra <75309640+saurabhdhingra@users.noreply.github.com>
2021-09-17 13:29:10 +0530

    First commit

---
+++ android/app/src/main/kotlin/eyesaver/com/eyesaver/MainActivity.kt
@@ -0,0 +1,6 @@
+package eyesaver.com.eyesaver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'Start',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
